e9d927d743f28bb81868213a11fb965d58e6f5a5
Jason Chen <69416879+jasonchen321@users.noreply.github.com>
2022-06-14 03:51:39 +0800

    Update my_first_dag.py

--- week2/my_first_dag.py
+++ week2/my_first_dag.py
@@ -1,54 +1,59 @@
-#import the library
+# import the libraries
+
 from datetime import timedelta
-#The DAG object; we'll need this to instantiate a DAG
+# The DAG object; we'll need this to instantiate a DAG
 from airflow import DAG
-#Operators; we need this to write tasks
+# Operators; we need this to write tasks!
 from airflow.operators.bash_operator import BashOperator
-#This makes scheduling easy
+# This makes scheduling easy
 from airflow.utils.dates import days_ago
 
-default_args={
-    'owner':'Jason',
-    'start_date':days_ago(0),
-    'email':['yoyoyo0321@hotmail.com'],
-    'email_on_failure':True,
-    'email_on_retry':True,
-    'retries':1,
-    'retry_delay':timedelta(minutes=5)
+#defining DAG arguments
+
+# You can override them on a per-task basis during operator initialization
+default_args = {
+    'owner': 'Jason',
+    'start_date': days_ago(0),
+    'email': ['yoyoyo0321@hotmail.com'],
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=5),
 }
 
-dag=DAG(
-dag_id='my-first-dag',
-default_args=default_args,
-description='My first ETL DAG',
-schedule_interval=timedelta(days=1)
+# defining the DAG
+
+# define the DAG
+dag = DAG(
+    'my-first-dag',
+    default_args=default_args,
+    description='My first DAG',
+    schedule_interval=timedelta(days=1),
 )
 
-#define the tasks
+# define the tasks
 
-#define the first task named extract
-extract=BashOperator(
+# define the first task
+
+extract = BashOperator(
     task_id='extract',
-    bash_command='cut -d":" -f1,3,6 /etc/passwd > extracted-data.txt'
-    dag=dag
+    bash_command='cut -d":" -f1,3,6 /etc/passwd > extracted-data.txt',
+    dag=dag,
 )
 
-#define the second task named transform
-transform_and_load=BashOperator(
+
+# define the second task
+transform_and_load = BashOperator(
     task_id='transform',
-    bash_command='tr ":""," < extracted-data.txt > transformed-data.csv'
-    dag=dag
+    bash_command='tr ":" "," < extracted-data.txt > transformed-data.csv',
+    dag=dag,
 )
 
-# #define the third task named load
-# load=BashOperator(
-#     task_id='load',
-#     bash_command='echo "load"',
-#     dag=dag
-# )
 
 # task pipeline
 extract >> transform_and_load
+
+
 
 #-------------------------------------Terminal----------------------------------------#
 #copy dag to AIRFLOW
